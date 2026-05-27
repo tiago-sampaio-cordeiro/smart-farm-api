97e808b765c0057fe8dc849b624646018babd2b6
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-05-27 19:21:11 -0300

    Updated app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,9 +6,11 @@ import { UsersModule } from './users/users.module';
 import { FarmsModule } from './farms/farms.module';
 import { SensorsModule } from './sensors/sensors.module';
 import { MeasurementsModule } from './measurements/measurements.module';
+import { AlertsModule } from './alerts/alerts.module';
+import { ThresholdsModule } from './thresholds/thresholds.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, FarmsModule, SensorsModule, MeasurementsModule],
+  imports: [DatabaseModule, UsersModule, FarmsModule, SensorsModule, MeasurementsModule, AlertsModule, ThresholdsModule],
   controllers: [AppController],
   providers: [AppService],
 })
