a94f14937986ad1d2c556b26935abba25294c92d
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-05-27 18:34:14 -0300

    Updated app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { FarmsModule } from './farms/farms.module';
+import { SensorsModule } from './sensors/sensors.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [DatabaseModule, UsersModule, FarmsModule, SensorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
