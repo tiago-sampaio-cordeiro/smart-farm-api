e26b9bfacf57c0a8c7f85e014fe6afd49d58cc43
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-05-27 21:27:10 -0300

    Added validator to measurements

--- apps/api/src/measurements/measurements.controller.ts
+++ apps/api/src/measurements/measurements.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, HttpCode, Param, Post, Query } from '@nestjs/common';
 import { MeasurementsService } from './measurements.service';
 import { Measurement } from 'src/Interfaces/measurement.interface';
+import { CreateMeasurementDto } from './dto/create-measurement.dto';
 
 @Controller('measurements')
 export class MeasurementsController {
@@ -8,8 +9,8 @@ export class MeasurementsController {
 
     @Post()
     @HttpCode(201)
-    create(@Body() body: Measurement) {
-        return this.measurementsService.create(body);
+    create(@Body() body: CreateMeasurementDto) {
+        return this.measurementsService.create(body as Measurement);
     }
 
     @Get()
